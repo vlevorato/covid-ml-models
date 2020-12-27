c0cb44967e72f9dc2e575229ea8a5f381d02554d
vincentlevorato <vincent.levorato@gmail.com>
2020-12-27 14:51:05 +0100

    add dummy_function (bis)

--- workflow/workflow_source_data.py
+++ workflow/workflow_source_data.py
@@ -17,18 +17,18 @@ task_start_import = DummyOperator(task_id='Start_source_data_import',
 
 input_owid_data_unit = DataInputFileUnit(data_paths['source_data_owid'])
 output_owid_data_unit = DataOutputFileUnit(data_paths['raw_data_path'] + '/owid_data.csv', index=False)
-task_owid_import = DataOperator(task_id='Import_OWID_data',
-                                operation_function=dummy_function,
+task_owid_import = DataOperator(operation_function=dummy_function,
                                 input_unit=input_owid_data_unit,
                                 output_unit=output_owid_data_unit,
+                                task_id='Import_OWID_data',
                                 dag=dag)
 
 input_datagov_data_unit = DataInputFileUnit(data_paths['source_data_gov'])
 output_datagov_data_unit = DataOutputFileUnit(data_paths['raw_data_path'] + '/datagov_data.csv', index=False)
-task_datagov_import = DataOperator(task_id='Import_DataGov_data',
-                                   operation_function=dummy_function,
+task_datagov_import = DataOperator(operation_function=dummy_function,
                                    input_unit=input_datagov_data_unit,
                                    output_unit=output_datagov_data_unit,
+task_id='Import_DataGov_data',
                                    dag=dag)
 
 task_start_import.set_downstream(task_owid_import)
