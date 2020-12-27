afc74b0f48db539ad2bc03ff7aad29e250a9e4a9
vincentlevorato <vincent.levorato@gmail.com>
2020-12-27 14:42:32 +0100

    update env vars names

--- covid_ml/config/env_vars.py
+++ covid_ml/config/env_vars.py
@@ -8,9 +8,9 @@ Using dictionary for config variables to be able to change way to assign values.
 
 config_variables = dict()
 env_list = [
-    'PROJECT_PATH',
-    'DATA_PATH',
-    'MODEL_PATH'
+    'COVIDML_PROJECT_PATH',
+    'COVIDML_DATA_PATH',
+    'COVIDML_MODEL_PATH'
 ]
 
 for env_var in env_list:
