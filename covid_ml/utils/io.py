11d7c168021c85e80413e36ad99b3e7153f25dff
vincentlevorato <vincent.levorato@gmail.com>
2020-12-27 14:48:26 +0100

    add dummy_function

---
+++ covid_ml/utils/io.py
@@ -0,0 +1,2 @@
+def dummy_function(dataframe):
+    return dataframe
--- workflow/workflow_source_data.py
+++ workflow/workflow_source_data.py
@@ -4,6 +4,7 @@ from dsbox.operators.data_operator import DataOperator
 from dsbox.operators.data_unit import DataInputFileUnit, DataOutputFileUnit
 
 from covid_ml.config.commons import dag_args, data_paths
+from covid_ml.utils.io import dummy_function
 
 dag = DAG(dag_id='covidml_source_data_import',
           default_args=dag_args,
@@ -17,6 +18,7 @@ task_start_import = DummyOperator(task_id='Start_source_data_import',
 input_owid_data_unit = DataInputFileUnit(data_paths['source_data_owid'])
 output_owid_data_unit = DataOutputFileUnit(data_paths['raw_data_path'] + '/owid_data.csv', index=False)
 task_owid_import = DataOperator(task_id='Import_OWID_data',
+                                operation_function=dummy_function,
                                 input_unit=input_owid_data_unit,
                                 output_unit=output_owid_data_unit,
                                 dag=dag)
@@ -24,6 +26,7 @@ task_owid_import = DataOperator(task_id='Import_OWID_data',
 input_datagov_data_unit = DataInputFileUnit(data_paths['source_data_gov'])
 output_datagov_data_unit = DataOutputFileUnit(data_paths['raw_data_path'] + '/datagov_data.csv', index=False)
 task_datagov_import = DataOperator(task_id='Import_DataGov_data',
+                                   operation_function=dummy_function,
                                    input_unit=input_datagov_data_unit,
                                    output_unit=output_datagov_data_unit,
                                    dag=dag)
