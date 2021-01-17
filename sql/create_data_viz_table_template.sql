49472b11b0d258bd718db1ab96d5a8be1dc55313
vincentlevorato <vincent.levorato@gmail.com>
2021-01-18 00:41:08 +0100

    update sql table viz creation

--- sql/create_data_viz_table_template.sql
+++ sql/create_data_viz_table_template.sql
@@ -5,6 +5,8 @@ as
 
   select
   historical_data.*,
+  SUM(nouveaux_patients_hospitalises) OVER (ORDER BY historical_data.date) as total_patients_hospitalises,
+  SUM(nouveaux_patients_reanimation) OVER (ORDER BY historical_data.date) as total_patients_reanimation,
   {2}
   from `{0}.historical_data` as historical_data
   {3}
--- sql/create_feature_viz_table.sql
+++ sql/create_feature_viz_table.sql
@@ -2,16 +2,31 @@ CREATE OR REPLACE TABLE
   {0}.viz_feature_contribution AS (
   SELECT
     feature_contribution_last.*,
-    (ABS(feature_contribution_last.importance) / feature_contribution_last_tot.total_importance) AS importance_proportion
+    ref_features.libelle AS feature_libelle,
+    ref_models.libelle AS model_libelle,
+    ref_cols.libelle AS target_libelle,
+    (ABS(feature_contribution_last.importance) / sum_importance) AS importance_proportion
   FROM
     `{0}.feature_contribution_last` AS feature_contribution_last
-  INNER JOIN (
+  LEFT JOIN (
     SELECT
-      SUM(ABS(importance)) AS total_importance,
+      SUM(ABS(importance)) AS sum_importance,
       target
     FROM
       `{0}.feature_contribution_last`
     GROUP BY
       target ) AS feature_contribution_last_tot
   ON
-    feature_contribution_last.target = feature_contribution_last_tot.target )
+    feature_contribution_last.target = feature_contribution_last_tot.target
+  LEFT JOIN
+    `{0}.ref_features` AS ref_features
+  ON
+    feature_contribution_last.feature = ref_features.variable
+  LEFT JOIN
+    `{0}.ref_models` AS ref_models
+  ON
+    feature_contribution_last.model = ref_models.variable
+  LEFT JOIN
+    `{0}.ref_cols` AS ref_cols
+  ON
+    feature_contribution_last.target = ref_cols.variable )
